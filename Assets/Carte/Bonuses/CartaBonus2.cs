ad01775d28e9bdc3b285c5e745285a5f7371d3c8
KumaCod3 <kumacod3@proton.me>
2024-11-16 15:46:50 +0100

    bonus vita + iniziati mostri 4 5 6

---
+++ Assets/Carte/Bonuses/CartaBonus2.cs
@@ -0,0 +1,21 @@
+public class CartaBonus2: baseCarta
+{
+	public int vita;
+
+	private void Start()
+	{
+		gameObject.GetComponent<GestCarta>().cambia1("+" + vita);
+		gameObject.GetComponent<GestCarta>().cambia4("+1");
+		tipo = "bonus";
+		nome = "Pozione Vita";
+		descrizione = "Guadagni 2 punto vita e 1 punto movimento.";
+	}
+
+	public override void action()
+	{
+		base.action();
+		PopinoController.vita += vita;
+		GameManager.punti += 1;
+		Destroy(gameObject);
+	}
+}
--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -6,7 +6,7 @@ public class TODO: MonoBehaviour
 	 * sistema salvataggio e levelUP
 	 *		mancano da decidere bonus liveli e aspetto!
 	 * 
-	 * fai altri mostri/ bonus
+	 * fai altri mostri 4 5 e 6
 	 * 	 
 	 * altre scene
 	 * 
